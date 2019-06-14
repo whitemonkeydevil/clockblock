e6254cb40336f7c66bc210f5d95aa5f91143e932
app <app@siesgst.local>
2019-06-14 17:36:54 +0530

    clock block

---
+++ src/Locax/Locax.js
@@ -0,0 +1,18 @@
+import React,{Component} from "react"
+
+class Locax extends Component{
+
+    render() {
+       let dieTime= setTimeout(() => {
+        
+        },60000);
+        count={}
+        return(
+               <div> You page will close in:{dieTime} 
+                </div>
+        )
+    }
+}
+
+
+export default Locax;
